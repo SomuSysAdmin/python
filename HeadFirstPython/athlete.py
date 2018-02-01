11a8f6388736f625918364d244379b027c0fbde1
SomuSysAdmin <somu.sysadm@gmail.com>
2018-02-01 12:21:44 +0530

    Introduced list comprehension (transformations).

--- HeadFirstPython/athlete.py
+++ HeadFirstPython/athlete.py
@@ -26,28 +26,43 @@ try:
             open('athleteTraining/mikey.txt') as mikeyData, open('athleteTraining/sarah.txt') as sarahData:
         for line in jamesData:
             for time in line.strip().split(','):        # This is called Method chaining.
-                james.append(sanitize(time))
+                james.append(time)
 
         for line in julieData:
             for time in line.strip().split(','):
-                julie.append(sanitize(time))
+                julie.append(time)
 
         for line in mikeyData:
-            for time in line.strip().split(','):
-                mikey.append(sanitize(time))
+            mikey = line.strip().split(',')     # Does the same thing as above, since a list is returned by split().
 
         for line in sarahData:
-            for time in line.strip().split(','):
-                sarah.append(sanitize(time))
+            sarah = line.strip().split(',')
 
 except Exception as err:
     print("File Error : " + str(err))
 
-if not 'james' in locals() and 'julie' in locals() and 'mikey'  in locals() and 'sarah' in locals():
+if not 'james' in locals() and 'julie' in locals() and 'mikey' in locals() and 'sarah' in locals():
     print("Unable to add the data to the lists!")
     exit(1)
 
-print("Sorted Times for James : " + str(sorted(james)))     # This is called function chaining.
-print("Sorted Times for Julie : " + str(sorted(julie)))
-print("Sorted Times for Mikey : " + str(sorted(mikey)))
-print("Sorted Times for Sarah : " + str(sorted(sarah)))
+# Sorted lists below:
+
+clean_james = []
+for time in james:
+    clean_james.append(sanitize(time))
+
+clean_julie = []
+for time in julie:
+    clean_julie.append(sanitize(time))
+"""
+This operation (and others like it) are called list transformation. The need to transform lists, i.e., created a new 
+list from the processed data from an existing list, is so frequent, that python provides a set of tools called *list 
+comprehensions* to do it.  The same code as above can be written with list comprehensions as:
+"""
+clean_mikey = [sanitize(time) for time in mikey]
+clean_sarah = [sanitize(time) for time in sarah]
+
+print("Sorted Times for James : " + str(sorted(clean_james)))     # This is called function chaining.
+print("Sorted Times for Julie : " + str(sorted(clean_julie)))
+print("Sorted Times for Mikey : " + str(sorted(clean_mikey)))
+print("Sorted Times for Sarah : " + str(sorted(clean_sarah)))
