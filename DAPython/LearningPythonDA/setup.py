e48643c8229b7378b61f1c3c24f20e7e504978bc
SomuSysAdmin <somu.sysadm@gmail.com>
2018-04-08 00:36:16 +0530

    Installed dependencies for venv.

--- DAPython/LearningPythonDA/setup.py
+++ DAPython/LearningPythonDA/setup.py
@@ -1,1 +1,20 @@
 from setuptools import setup, find_packages
+
+setup (
+    name = "Data Analysis",
+    version = '0.0.1',
+    url = "https://github.com/SomuSysAdmin/python/tree/master/DAPython/LearningPythonDA",
+    author = 'SomuSysAdmin',
+    author_email= 'letsomuknow@gmail.com',
+    packages = find_packages(),
+    install_requires = ['PyQt5',
+                        'pandas',
+                        'sqlalchemy',
+                        'nltk',
+                        'numpy',
+                        "jupyter",
+                        'python-twitter'],
+    entry_points = {},
+    extras_require = {'dev' : ['flake8',]},
+
+)
